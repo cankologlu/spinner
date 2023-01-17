b69defe5888ba2d4fbd7a3554ee817c02d58781a
Can Kologlu <mckologlu@gmail.com>
2023-01-17 17:12:59 -0500

    spinner with timeout function

---
+++ spinner1.js
@@ -0,0 +1,133 @@
+process.stdout.write("hello from spinne11.js \rheyyy\n");
+
+setTimeout(() => {
+  process.stdout.write("\r|    ");
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write("\r/    ");
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write("\r-    ");
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write("\r\\    ");
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write("\r|    ");
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write("\r/    ");
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write("\r-    ");
+}, 1300);
+
+setTimeout(() => {
+  process.stdout.write("\r\\    ");
+}, 1500);
+
+setTimeout(() => {
+  process.stdout.write("\r|    ");
+}, 1700);
+
+setTimeout(() => {
+  process.stdout.write("\r/    ");
+}, 1900);
+
+setTimeout(() => {
+  process.stdout.write("\r-    ");
+}, 2100);
+
+setTimeout(() => {
+  process.stdout.write("\r\\    ");
+}, 2300);
+
+setTimeout(() => {
+  process.stdout.write("\r|    ");
+}, 2500);
+
+setTimeout(() => {
+  process.stdout.write("\r/    ");
+}, 2700);
+
+setTimeout(() => {
+  process.stdout.write("\r-    ");
+}, 2900);
+
+setTimeout(() => {
+  process.stdout.write("\r\\    ");
+}, 3100);
+
+setTimeout(() => {
+  process.stdout.write("\r|    ");
+}, 3300);
+
+setTimeout(() => {
+  process.stdout.write("\r/    ");
+}, 3500);
+
+setTimeout(() => {
+  process.stdout.write("\r-    ");
+}, 3700);
+
+setTimeout(() => {
+  process.stdout.write("\r\\    ");
+}, 3900);
+
+setTimeout(() => {
+  process.stdout.write("\r|    ");
+}, 4100);
+
+setTimeout(() => {
+  process.stdout.write("\r/    ");
+}, 4300);
+
+setTimeout(() => {
+  process.stdout.write("\r-    ");
+}, 4500);
+
+setTimeout(() => {
+  process.stdout.write("\r\\    ");
+}, 4700);
+
+setTimeout(() => {
+  process.stdout.write("\r|    ");
+}, 4900);
+
+setTimeout(() => {
+  process.stdout.write("\r/    ");
+}, 5100);
+
+setTimeout(() => {
+  process.stdout.write("\r-    ");
+}, 5300);
+
+setTimeout(() => {
+  process.stdout.write("\r\\    ");
+}, 5500);
+
+setTimeout(() => {
+  process.stdout.write("\r|    ");
+}, 5700);
+
+setTimeout(() => {
+  process.stdout.write("\r/    ");
+}, 5900);
+
+setTimeout(() => {
+  process.stdout.write("\r-    ");
+}, 6100);
+
+setTimeout(() => {
+  process.stdout.write("\r\\    ");
+}, 6300);
+
+setTimeout(() => {
+  process.stdout.write("\r|\n");
+}, 6500);
